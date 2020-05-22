9d5b078dd4bf082d0b09d4d5b73563faf2319ff0
Madeorsk <madeorsk@protonmail.com>
2020-05-22 16:36:25 +0200

    Set default cookie timeout to more than 60 seconds
    Default cookie timeout was `time()` + 60 seconds, which means that this was not really usable in production (users needed to fill the form in one minute, otherwise the CSRF token was considered as invalid!).

--- src/Psecio/Csrf/Storage/Cookie.php
+++ src/Psecio/Csrf/Storage/Cookie.php
@@ -7,7 +7,7 @@ class Cookie extends \Psecio\Csrf\Storage
 
     public function save($key, $code)
     {
-        $timeout = ($this->getConfig('timeout') === null) ? time() + 60 : $this->getConfig('timeout');
+        $timeout = ($this->getConfig('timeout') === null) ? 0 : $this->getConfig('timeout');
         $secure = (!empty($_SERVER['HTTPS']));
 
         setcookie($key, $code, $timeout, '/', $_SERVER['SERVER_NAME'], $secure, true);
